e5efba3014ee6232d9c25b5a125eec3c6bd9f32d
Jaskulap <jaskulap@gmail.com>
2023-12-10 21:29:53 +0100

    Pierwszy Commit

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,10 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.UIElements;
 using Button = UnityEngine.UI.Button;
 
 public class UIManager : MonoBehaviour
@@ -30,7 +26,7 @@ public class UIManager : MonoBehaviour
         loadPopUp.SetActive(false);
     }
 
-    
+
 
     public void ChangePopUp(GameObject newPopUp)
     {
